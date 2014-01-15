d7e0789b4fd1833a84490459d0953ea34750e50c
PiMastah <pimastah@googlemail.com>
2014-01-15 22:56:31 +0100

    some code review

--- app.js
+++ app.js
@@ -1,70 +1,83 @@
-var notesTotal = 0;
-var notesPerClick = 1;
-var songsOn = false;
+(function($){
+	$(document).on('ready', function() {
+		var notesTotal = 0;
+		var notesPerClick = 1;
+		var songsOn = false;
 
-var coverOn = false;
-var coverOwned = 0;
-var coverEffect = 1;
-var coverCost = 10;
+		var coverOn = false;
+		var coverOwned = 0;
+		var coverEffect = 1;
+		var coverCost = 10;
 
-var showNotes = function() {
-    document.getElementById('notesTotal').innerHTML = 'Noten: ' + Math.round(notesTotal * 100) / 100;
-    document.getElementById('notesPerClick').innerHTML = 'Noten pro Klick: ' + Math.round(notesPerClick * 100) / 100;
-}
+		var showNotes = function() {
+		    document.getElementById('notesTotal').innerHTML = 'Noten: ' + Math.round(notesTotal * 100) / 100;
+		    document.getElementById('notesPerClick').innerHTML = 'Noten pro Klick: ' + Math.round(notesPerClick * 100) / 100;
+		}
 
-var showStatsTitle = function() {
-    document.getElementById('statsUpgrade').innerHTML = 'Upgrade';
-    document.getElementById('statsEffect').innerHTML = 'Gesamteffekt';
-    document.getElementById('statsOwned').innerHTML = 'In Besitz';
-}
+		var showStatsTitle = function() {
+		    document.getElementById('statsUpgrade').innerHTML = 'Upgrade';
+		    document.getElementById('statsEffect').innerHTML = 'Gesamteffekt';
+		    document.getElementById('statsOwned').innerHTML = 'In Besitz';
+		}
 
-var showSongs = function() {
-    document.getElementById('buySongsTitle').innerHTML = 'Songs';
-    document.getElementById('buySongsUpgrade').innerHTML = 'Upgrade';
-    document.getElementById('buySongsCost').innerHTML = 'Kosten';
-    document.getElementById('buySongsEffect').innerHTML = 'Gesamteffekt';
-}
+		var showSongs = function() {
+		    document.getElementById('buySongsTitle').innerHTML = 'Songs';
+		    document.getElementById('buySongsUpgrade').innerHTML = 'Upgrade';
+		    document.getElementById('buySongsCost').innerHTML = 'Kosten';
+		    document.getElementById('buySongsEffect').innerHTML = 'Gesamteffekt';
+		}
 
-var showStatsCover = function() {
-    showStatsTitle();
-    document.getElementById('coverName').innerHTML = 'Cover';
-    document.getElementById('coverEffect').innerHTML = '+' + Math.round(coverEffect * 100) / 100 + '/Klick';
-    document.getElementById('coverOwned').innerHTML = Math.round(coverOwned * 100) / 100;
-}
+		var showStatsCover = function() {
+		    showStatsTitle();
+		    document.getElementById('coverName').innerHTML = 'Cover';
+		    document.getElementById('coverEffect').innerHTML = '+' + Math.round(coverEffect * 100) / 100 + '/Klick';
+		    document.getElementById('coverOwned').innerHTML = Math.round(coverOwned * 100) / 100;
+		}
 
-var showBuyCover = function() {
-    document.getElementById('coverBuyName').innerHTML = '<a href="#" onClick="buyCover();">Cover Version</a>';
-    document.getElementById('coverCost').innerHTML = Math.round(coverCost * 100) / 100;
-    document.getElementById('coverBuyEffect').innerHTML = '+' + Math.round(coverEffect * 100) / 100 + '/Klick';
-}
+		var showBuyCover = function() {
+		    document.getElementById('coverBuyName').innerHTML = '<a href="#" id="buyCover">Cover Version</a>';
+		    document.getElementById('coverCost').innerHTML = Math.round(coverCost * 100) / 100;
+		    document.getElementById('coverBuyEffect').innerHTML = '+' + Math.round(coverEffect * 100) / 100 + '/Klick';
+		}
 
-var clickRecord = function() {
-    notesTotal = notesTotal + notesPerClick;
+		var clickRecord = function() {
+		    notesTotal = notesTotal + notesPerClick;
 
-    if (notesTotal >= coverCost || songsOn) {
-        showSongs();
-    }
+		    if (notesTotal >= coverCost || songsOn) {
+		        showSongs();
+		    }
 
-    if (notesTotal >= coverCost || coverOn) {
-        showBuyCover();
-    };
+		    if (notesTotal >= coverCost || coverOn) {
+		        showBuyCover();
+		    };
 
-    showNotes();
-}
+		    showNotes();
+		}
 
 
-var buyCover = function() {
-    if (notesTotal >= coverCost) {
-        notesTotal = notesTotal - coverCost;
-        notesPerClick = notesPerClick + coverEffect;
-        coverCost = coverCost + (coverCost*0.05);
-        coverOwned++;
+		var buyCover = function() {
+		    if (notesTotal >= coverCost) {
+		        notesTotal = notesTotal - coverCost;
+		        notesPerClick = notesPerClick + coverEffect;
+		        coverCost = coverCost + (coverCost*0.05);
+		        coverOwned++;
 
-        showNotes();
-        coverCost++;
-        coverOn = true;
-        showStatsCover();
-        coverEffect = coverEffect + coverEffect*0.05;
-        showBuyCover();
-    }
-}
+		        showNotes();
+		        coverCost++;
+		        coverOn = true;
+		        showStatsCover();
+		        coverEffect = coverEffect + coverEffect*0.05;
+		        showBuyCover();
+		    }
+		}
+
+		$('#clickRecord').on('click', function (e) {
+			e.preventDefault();
+			clickRecord();
+		});
+		$('body').on('click', '#buyCover', function (e) {
+			e.preventDefault();
+			buyCover();
+		});
+	});
+})(jQuery);
